22e427a99451287bbe5aa9fc67ff9ce73121e037
Felipe Viana <viana@ucf.edu>
2019-02-25 18:33:21 -0500

    updated StressIntensity and ParisLaw to allow for Sequential model

--- samples/cumulative_damage/airplane_fleet_crack_growth/model.py
+++ samples/cumulative_damage/airplane_fleet_crack_growth/model.py
@@ -55,6 +55,17 @@ sys.path.append('../../../')
 
 from pinn.layers import ParisLaw, CumulativeDamageCell, StressIntensityRange
 
+def build_model():
+    model = tf.keras.Sequential([
+            tf.keras.layers.Dense(1, input_shape=[None, 2]),
+            tf.keras.layers.Dense(1)
+            ])
+    optimizer = tf.keras.optimizers.RMSprop(0.001)
+    model.compile(loss='mean_squared_error',
+                  optimizer=optimizer,
+                  metrics=['mean_absolute_error', 'mean_squared_error'])
+    return model
+
 def create_model(dkLayer, C, m, batch_input_shape, a0RNN, myDtype, return_sequences = False, unroll = False):
     
     da_input_shape = dkLayer.get_output_shape_at(-1)
@@ -87,36 +98,37 @@ def create_model(dkLayer, C, m, batch_input_shape, a0RNN, myDtype, return_sequen
 
 def create_physics_model(F, C, m, batch_input_shape, a0RNN, myDtype, return_sequences = False, unroll = False):
 
-    dK_input_shape = tensor_shape.TensorShape([None, 2])
-    dkLayer = StressIntensityRange(input_shape = dK_input_shape, dtype = myDtype)
-    dkLayer.build(input_shape = dK_input_shape)
-    dkLayer.set_weights([np.asarray([F], dtype=dkLayer.dtype)])
-    dkLayer.trainable = False
-
-#    da_input_shape = tensor_shape.TensorShape([batch_input_shape[0], 1])
-#    daLayer = ParisLaw(input_shape = da_input_shape, dtype = myDtype)
-#    daLayer.build(input_shape = da_input_shape)
-#    daLayer.set_weights([np.asarray([C, m], dtype=daLayer.dtype)])
-#    daLayer.trainable = False
-#    	
-#    PINNhybrid = tf.keras.Sequential()
-#    PINNhybrid.add(dkLayer)
-#    PINNhybrid.add(daLayer)
-#
-#    "-------------------------------------------------------------------------"
-#    CDMCellHybrid = CumulativeDamageCell(model = PINNhybrid,
-#                                       batch_input_shape = batch_input_shape,
-#                                       dtype = myDtype,
-#                                       initial_damage = a0RNN)
-#     
-#    CDMRNNhybrid = tf.keras.layers.RNN(cell = CDMCellHybrid,
-#                                       return_sequences = return_sequences,
-#                                       return_state = False,
-#                                       batch_input_shape = batch_input_shape,
-#                                       unroll = unroll)
-#
-#    model = tf.keras.Sequential()
-#    model.add(CDMRNNhybrid)
-#    model.compile(loss='mse', optimizer=tf.keras.optimizers.RMSprop(1e-12), metrics=['mae'])
+    dk_input_shape = tensor_shape.TensorShape([2])
     
-    return dkLayer
+    dkLayer = StressIntensityRange(input_shape = dk_input_shape, dtype = myDtype)
+    dkLayer.build(input_shape = dk_input_shape)
+    dkLayer.set_weights([np.asarray([F], dtype = dkLayer.dtype)])
+    dkLayer.trainable = False
+    
+    da_input_shape = tensor_shape.TensorShape([None, 1])
+    daLayer = ParisLaw(input_shape = da_input_shape, dtype = myDtype)
+    daLayer.build(input_shape = da_input_shape)
+    daLayer.set_weights([np.asarray([C, m], dtype=daLayer.dtype)])
+    daLayer.trainable = False
+
+    PINNhybrid = tf.keras.Sequential()
+    PINNhybrid.add(dkLayer)
+    PINNhybrid.add(daLayer)
+
+    "-------------------------------------------------------------------------"
+    CDMCellHybrid = CumulativeDamageCell(model = PINNhybrid,
+                                       batch_input_shape = batch_input_shape,
+                                       dtype = myDtype,
+                                       initial_damage = a0RNN)
+     
+    CDMRNNhybrid = tf.keras.layers.RNN(cell = CDMCellHybrid,
+                                       return_sequences = return_sequences,
+                                       return_state = False,
+                                       batch_input_shape = batch_input_shape,
+                                       unroll = unroll)
+
+    model = tf.keras.Sequential()
+    model.add(CDMRNNhybrid)
+    model.compile(loss='mse', optimizer=tf.keras.optimizers.RMSprop(1e-12), metrics=['mae'])
+    
+    return model
