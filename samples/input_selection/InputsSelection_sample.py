8574fd1b4a1b657ebd2010cc027c33c5a3832bcb
Yigit Yucesan <yucesan@knights.ucf.edu>
2019-02-27 14:44:34 -0500

    Sample is implemented

--- samples/input_selection/InputsSelection_sample.py
+++ samples/input_selection/InputsSelection_sample.py
@@ -8,38 +8,39 @@ import numpy as np
 import tensorflow as tf
 
 from tensorflow.keras.layers import Dense
-from tensorflow.python.framework import ops
 # =============================================================================
 # Function
 # =============================================================================
-def inputsSelection(inputs_shape, ndex):
-    input_mask = np.zeros(inputs_shape[-1], dtype = int)
-    input_mask[ndex] = 1
-    input_mask = np.diag(input_mask)
+def inputsSelection(inputs, ndex):
+    input_mask = np.zeros([inputs.shape[-1], len(ndex)])
+    for i in range(inputs.shape[-1]):
+        for v in range(len(ndex)):
+            if i == v:
+                input_mask[i,v] = 1
         
-    dL = Dense(inputs_shape[-1], activation = None, input_shape = inputs_shape, 
+    dL = Dense(len(ndex), activation = None, input_shape = inputs.shape, 
                use_bias = False)
-    dL.build(input_shape = inputs_shape)
+    dL.build(input_shape = inputs.shape)
     dL.set_weights([input_mask])
     dL.trainable = False
     return dL
 
-def create_model(ndex, batch_input_shape):
-    dLSelction = inputsSelection(batch_input_shape, ndex)
+
+def create_model(input_array, ndex):
+    dLSelction = inputsSelection(input_array, ndex)
     model = tf.keras.Sequential()
     model.add(dLSelction)
     return model
+
 # =============================================================================
 # Main
 # =============================================================================
 np.random.seed(123)
-myDtype = tf.float32
 
 input_array = np.random.random((10,5))
-input_array = ops.convert_to_tensor(input_array, dtype = myDtype)
+input_shape = input_array.shape
 ndex = np.asarray([0,2,4])
 
-batch_input_shape = input_array.shape
-
-test_model = create_model(ndex, batch_input_shape)
-test_model.predict_on_batch(input_array)
+test_model = create_model(input_array, ndex)
+out = test_model.predict(input_array.reshape((1,10,5)))
+print(out)
