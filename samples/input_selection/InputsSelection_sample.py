cf9ec1d093735ca27ecaca235e7939e005235d2d
Arinan Dourado <arinandourado@knights.ucf.edu>
2019-02-27 13:18:33 -0500

    Updated version

--- samples/input_selection/InputsSelection_sample.py
+++ samples/input_selection/InputsSelection_sample.py
@@ -12,17 +12,23 @@ from tensorflow.python.framework import ops
 # =============================================================================
 # Function
 # =============================================================================
-def inputsSelection(inputs, ndex):
-    input_mask = np.zeros(inputs.get_shape()[-1], dtype = int)
+def inputsSelection(inputs_shape, ndex):
+    input_mask = np.zeros(inputs_shape[-1], dtype = int)
     input_mask[ndex] = 1
     input_mask = np.diag(input_mask)
         
-    dL = Dense(inputs.get_shape()[-1], activation = None, input_shape = inputs.shape, 
+    dL = Dense(inputs_shape[-1], activation = None, input_shape = inputs_shape, 
                use_bias = False)
-    dL.build(input_shape = inputs.shape)
+    dL.build(input_shape = inputs_shape)
     dL.set_weights([input_mask])
     dL.trainable = False
     return dL
+
+def create_model(ndex, batch_input_shape):
+    dLSelction = inputsSelection(batch_input_shape, ndex)
+    model = tf.keras.Sequential()
+    model.add(dLSelction)
+    return model
 # =============================================================================
 # Main
 # =============================================================================
@@ -33,4 +39,7 @@ input_array = np.random.random((10,5))
 input_array = ops.convert_to_tensor(input_array, dtype = myDtype)
 ndex = np.asarray([0,2,4])
 
-dLSample = inputsSelection(input_array, ndex)
+batch_input_shape = input_array.shape
+
+test_model = create_model(ndex, batch_input_shape)
+test_model.predict_on_batch(input_array)
