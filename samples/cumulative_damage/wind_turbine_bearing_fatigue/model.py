26e533a76ed24512361a7bc13075bf36e4597601
Yigit Yucesan <yucesan@knights.ucf.edu>
2019-03-13 17:08:39 -0400

    Initial commit for wtg bearing fatigue damage model without grease effect

---
+++ samples/cumulative_damage/wind_turbine_bearing_fatigue/model.py
@@ -0,0 +1,124 @@
+# ______          _           _     _ _ _     _   _      
+# | ___ \        | |         | |   (_) (_)   | | (_)     
+# | |_/ / __ ___ | |__   __ _| |__  _| |_ ___| |_ _  ___ 
+# |  __/ '__/ _ \| '_ \ / _` | '_ \| | | / __| __| |/ __|
+# | |  | | | (_) | |_) | (_| | |_) | | | \__ \ |_| | (__ 
+# \_|  |_|  \___/|_.__/ \__,_|_.__/|_|_|_|___/\__|_|\___|
+# ___  ___          _                 _                  
+# |  \/  |         | |               (_)                 
+# | .  . | ___  ___| |__   __ _ _ __  _  ___ ___         
+# | |\/| |/ _ \/ __| '_ \ / _` | '_ \| |/ __/ __|        
+# | |  | |  __/ (__| | | | (_| | | | | | (__\__ \        
+# \_|  |_/\___|\___|_| |_|\__,_|_| |_|_|\___|___/        
+#  _           _                     _                   
+# | |         | |                   | |                  
+# | |     __ _| |__   ___  _ __ __ _| |_ ___  _ __ _   _ 
+# | |    / _` | '_ \ / _ \| '__/ _` | __/ _ \| '__| | | |
+# | |___| (_| | |_) | (_) | | | (_| | || (_) | |  | |_| |
+# \_____/\__,_|_.__/ \___/|_|  \__,_|\__\___/|_|   \__, |
+#                                                   __/ |
+#                                                  |___/ 
+#														  
+# MIT License
+# 
+# Copyright (c) 2019 Probabilistic Mechanics Laboratory
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# ==============================================================================
+
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+
+from tensorflow.python.framework import ops
+from tensorflow.keras.layers import Input, multiply
+
+import sys
+sys.path.append('../../../')
+
+from pinn.layers import CumulativeDamageCell
+from pinn.layers.physics import SNCurve
+from pinn.layers.core import inputsSelection
+
+# Model
+def create_model(a, b, d0RNN, batch_input_shape, input_array, selectCycle, selectLoad, myDtype, return_sequences = False, unroll = False):
+    
+    filterCycleLayer = inputsSelection(input_array, selectCycle)
+    
+    filterLoadLayer = inputsSelection(input_array, selectLoad)
+    
+    n = batch_input_shape[0]
+    sn_input_shape = (n,2)
+    
+    SNLayer = SNCurve(input_shape = sn_input_shape, dtype = myDtype)
+    SNLayer.build(input_shape = sn_input_shape)
+    SNLayer.set_weights([np.asarray([a,b], dtype = SNLayer.dtype)])
+    SNLayer.trainable = False
+    
+    multiplyLayer = multiply([SNLayer,filterCycleLayer])
+    
+    PINNphysics = tf.keras.Sequential()
+    PINNphysics.add(filterLoadLayer)
+    PINNphysics.add(SNLayer)
+    PINNphysics.add(multiplyLayer)
+    
+    "-------------------------------------------------------------------------"
+    CDMCellHybrid = CumulativeDamageCell(model = PINNphysics,
+                                       batch_input_shape = batch_input_shape,
+                                       dtype = myDtype,
+                                       initial_damage = d0RNN)
+     
+    CDMRNNhybrid = tf.keras.layers.RNN(cell = CDMCellHybrid,
+                                       return_sequences = return_sequences,
+                                       return_state = False,
+                                       batch_input_shape = batch_input_shape,
+                                       unroll = unroll)
+    
+    model = tf.keras.Sequential()
+    model.add(CDMRNNhybrid)
+    model.compile(loss='mse', optimizer=tf.keras.optimizers.RMSprop(1e-12), metrics=['mae'])
+    return model
+
+
+if __name__ == "__main__":
+
+    # Preliminaries
+    myDtype = tf.float32
+    
+    a = -10/3                  # Slope of linearized SN-Curve in log10-log10 space
+    b = 12.594                 # Interception of linearized SN-Curve in log10-log10 space
+    d0RNN = 0
+    
+    # Inputs
+    df = pd.read_csv('Cycles.csv', index_col = None)
+    cycFleet = np.transpose(np.asarray(df))
+    
+    df = pd.read_csv('DynamicLoad.csv', index_col = None)
+    PFleet = np.transpose(np.asarray(df))
+    nFleet, n10min = PFleet.shape
+    
+    inputArray = np.dstack((cycFleet, PFleet))
+    selectCycle = [1]
+    selectLoad = [2]
+    batch_input_shape = inputArray.shape
+    inputTensor = ops.convert_to_tensor(inputArray, dtype = myDtype)
+    
+    d0RNN = ops.convert_to_tensor(d0RNN * np.ones((inputArray.shape[0], 1)), dtype=myDtype)
+            
+    model = create_model(a, b, d0RNN, batch_input_shape, inputTensor, selectCycle, selectLoad, myDtype, return_sequences = True)
