7e0ba84816aa68e9453c687114ad61c37ac07ad4
renatogn <renato.gn@gmail.com>
2019-07-29 14:47:42 -0400

    Fix code style sn_curve

--- samples/cumulative_damage/sn_curve/run01_sample_sn_curve.py
+++ samples/cumulative_damage/sn_curve/run01_sample_sn_curve.py
@@ -47,36 +47,31 @@
 
 import numpy as np
 import pandas as pd
-#import tensorflow as tf
 
 import matplotlib.pyplot as plt
 
-#from tensorflow.python.framework import ops
-
-import sys
-sys.path.append('../../../')
 from model import create_model
 
-#--------------------------------------------------------------------------
+# --------------------------------------------------------------------------
 if __name__ == "__main__":
-    
+
     myDtype = 'float32'
 
-    #--------------------------------------------------------------------------
+    # --------------------------------------------------------------------------
     # Sequence for importing input loads for each asset as time series
-    df = pd.read_csv('Loads_1000cycles.csv', index_col = None, dtype=myDtype)
+    df = pd.read_csv('Loads_1000cycles.csv', index_col=None, dtype=myDtype)
     macArray = np.transpose(np.asarray(df))
     nFleet, nCycles = macArray.shape
-    avgLoads = np.linspace(100,500,nFleet,dtype=myDtype)
-    
-    #--------------------------------------------------------------------------
+    avgLoads = np.linspace(100, 500, nFleet, dtype=myDtype)
+
+    # --------------------------------------------------------------------------
     # Preliminary parameters
-    
+
     # The spherical bearing life equation used in this sample case is given in the reference:
     # "Y. A. Yucesan and F. A. C. Viana, ''Onshore wind turbine main bearing 
     # reliability and its implications in fleet management,'' in AIAA
     # SciTech Forum, (San Diego, USA), pp. AIAA 2019--1225, AIAA, 2019."
-    
+
     # The equation is N = a1*askf*(C/P)**(10/3)
     # Linearization of this equation in the log10-log10 space gives below linear equation parameters as:
     # a = -10/3, b = log10(a1) + log10(askf) + 10/3*log10(C)
@@ -86,14 +81,14 @@ if __name__ == "__main__":
     #        * C is the basic dynamic load rating (taken as 6000 kN for SKF 230/600 CAW33 model bearing)
     #        * P is the equivalent dynamic bearing load (cyclic input load)
     #        * Life cycles in millions for corresponding P
-    
-    a = -10/3                  # Slope of linearized SN-Curve in log10-log10 space
-    b = 12.594                 # Interception of linearized SN-Curve in log10-log10 space
-    N = a*np.log10(macArray)+b # Number of cycles for corresponding load
-    d = 1/10**N                # Delta damage
-    d0 = 0.                    # Initial damage
-    ndex = [1]                 # To filter load from inputs
-    #--------------------------------------------------------------------------
+
+    a = -10 / 3  # Slope of linearized SN-Curve in log10-log10 space
+    b = 12.594  # Interception of linearized SN-Curve in log10-log10 space
+    N = a * np.log10(macArray) + b  # Number of cycles for corresponding load
+    d = 1 / 10 ** N  # Delta damage
+    d0 = 0.  # Initial damage
+    ndex = [1]  # To filter load from inputs
+    # --------------------------------------------------------------------------
     # Sequence for damage history calculation
     dHistAll = []
     for mac in d:
@@ -104,47 +99,45 @@ if __name__ == "__main__":
             dHist.append(dmgCum)
         dHistAll.append(dHist)
     dHistAll = np.asarray(dHistAll, dtype=myDtype)
-    
+
     # Input loads tensor manipulations
     Sobs = macArray[:, :, np.newaxis]
     Sobs = np.log10(Sobs)
-#    Sobs = ops.convert_to_tensor(Sobs, dtype = myDtype)
     batch_input_shape = Sobs.shape
-    
-    #--------------------------------------------------------------------------
+
+    # --------------------------------------------------------------------------
     # Prediction sequence
-#    da0RNN = ops.convert_to_tensor(d0 * np.ones((Sobs.shape[0], 1)), dtype=myDtype)
     da0RNN = d0 * np.ones((Sobs.shape[0], 1), dtype=myDtype)
-        
-    model = create_model(a = a, b = b, batch_input_shape = batch_input_shape, ndex = ndex,
-                         da0RNN = da0RNN, myDtype = myDtype, return_sequences = True)
-    
-    results = model.predict_on_batch(Sobs)[:,:,0]
-    #--------------------------------------------------------------------------    
+
+    model = create_model(a=a, b=b, batch_input_shape=batch_input_shape, ndex=ndex,
+                         da0RNN=da0RNN, myDtype=myDtype, return_sequences=True)
+
+    results = model.predict_on_batch(Sobs)[:, :, 0]
+    # --------------------------------------------------------------------------
     # Plot damage history for all machines
-    fig  = plt.figure(1)
+    fig = plt.figure(1)
     fig.clf()
-    
-    plt.plot(np.transpose(np.repeat([range(nCycles)],nFleet,axis=0)), np.transpose(results))
-    
+
+    plt.plot(np.transpose(np.repeat([range(nCycles)], nFleet, axis=0)), np.transpose(results))
+
     plt.title('Damage History')
     plt.xlabel('Million Cycles')
     plt.ylabel('Damage')
-    plt.grid(which = 'both')
+    plt.grid(which='both')
     plt.show()
-    
+
     # Plot SN-Curve
-    fig  = plt.figure(2)
+    fig = plt.figure(2)
     fig.clf()
-    
-    plt.plot(1/dHistAll[:,-1], avgLoads,'b-', label = 'SN model')
-    plt.plot(1/results[:,-1], avgLoads,'r--', label = 'PINN')
-    
+
+    plt.plot(1 / dHistAll[:, -1], avgLoads, 'b-', label='SN model')
+    plt.plot(1 / results[:, -1], avgLoads, 'r--', label='PINN')
+
     plt.title('SN Curve')
     plt.xscale('log')
     plt.yscale('log')
     plt.xlabel('Million Cycles')
     plt.ylabel('Load (kN)')
-    plt.grid(which = 'both')
-    plt.legend(loc=0, facecolor = 'w')
+    plt.grid(which='both')
+    plt.legend(loc=0, facecolor='w')
     plt.show()
